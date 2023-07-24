a7f506066cc4bd2df5b4faebc0c891b7bf5e1094
Mayurp375 <mayurpolojwar1234@gmail.com>
2023-07-24 15:56:39 +0530

    can ablet to show data in center

--- src/app/fetch-page/fetch-page.component.ts
+++ src/app/fetch-page/fetch-page.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class FetchPageComponent implements OnInit {
 
   @Input() hero :any;
+ // addToCart: any ='https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngtree.com%2Fso%2Fadd-to-cart&psig=AOvVaw1Cv20kJbsu1KUvg8jR9iuX&ust=1690279786900000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCMjP6Y-Np4ADFQAAAAAdAAAAABAD';
   ngOnInit() {};
 
 }
